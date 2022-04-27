b812b7cb56096e4466f24c02bde816cfd8f94a1a
Marisol Rodriguez <maridrodriguez@gmail.com>
2022-04-27 18:47:09 -0400

    appending file to meet criteria for Activity: Web Game Part III

--- index.js
+++ index.js
@@ -1,51 +1,76 @@
-function newImage(url, left, bottom){
-    let image = document.createElement('img')
-    image.src = url
-    image.style.position = 'fixed'
-    image.style.left = left + 'px'
-    image.style.bottom = bottom + 'px'
-    document.body.append(image)
-    return image
+function newImage(url) {
+  let image = document.createElement("img");
+  image.src = url;
+  // image.style.position = 'fixed'
+  // image.style.left = left + 'px'
+  // image.style.bottom = bottom + 'px'
+  document.body.append(image);
+  return image;
 }
 
-newImage('assets/green-character.gif', 100, 250)
-newImage('assets/tree.png', 200, 450)
-newImage('assets/pillar.png', 350, 250)
-newImage('assets/pine-tree.png', 450, 350)
-newImage('assets/crate.png', 150, 350)
-newImage('assets/well.png', 500, 575)
+// function move(image, left, bottom){
+//     image.style.position = 'fixed'
+//     image.style.left = left + 'px'
+//     image.style.bottom = bottom + 'px'
+// }
 
+function move(image) {
+  image.style.position = "fixed";
 
-function newItem(url, left, bottom){
-    let item = newImage(url, left, bottom)
-    item.addEventListener('click', () => {
-        item.remove()
-        let inventoryItem = document.createElement('img')
-        inventoryItem.src = url;
-        inventory.append(inventoryItem)
-    })
-    return item
+  function moveToCoordinates(left, bottom) {
+    image.style.left = left + "px";
+    image.style.bottom = bottom + "px";
+  }
+  return {
+    to: moveToCoordinates,
+  };
 }
 
-newItem('assets/sword.png', 500, 555)
-newItem('assets/shield.png', 165, 335)
-newItem('assets/staff.png', 600, 250)
+// let greenCharacter = newImage('assets/green-character.gif')
+// let thingThatMoveReturns = move(greenCharacter)
+// thingThatMoveReturns.to
+// thingThatMoveReturns.to(100, 200)
+// thingThatMoveReturns.to(300, 300)
+// the above code lines 29-33 is condensed to one line of code below:
+move(newImage("assets/green-character.gif")).to(100, 250);
 
-function newInventory(){
-    let inventory = document.createElement('div')
-    inventory.style.position = 'fixed'
-    inventory.style.bottom = '0px';
-    inventory.style.left = '0px'
-    inventory.style.width = '100%'
-    inventory.style.height = '100px'
-    inventory.style.display = 'flex'
-    inventory.style.flexDirection = 'row'
-    inventory.style.alignItems = 'center'
-    inventory.style.justifyContent = 'space-evenly'
-    inventory.style.border = '2px solid black'
-    inventory.style.backgroundColor = 'brown'
-    document.body.append(inventory)
-    return inventory
+// newImage('assets/green-character.gif', 100, 250)
+// newImage('assets/tree.png', 200, 450)
+// newImage('assets/pillar.png', 350, 250)
+// newImage('assets/pine-tree.png', 450, 350)
+// newImage('assets/crate.png', 150, 350)
+// newImage('assets/well.png', 500, 575)
+
+function newItem(url, left, bottom) {
+  let item = newImage(url, left, bottom);
+  item.addEventListener("click", () => {
+    item.remove();
+    let inventoryItem = document.createElement("img");
+    inventoryItem.src = url;
+    inventory.append(inventoryItem);
+  });
+  return item;
 }
 
-const inventory = newInventory()
+// newItem('assets/sword.png', 500, 555)
+// newItem('assets/shield.png', 165, 335)
+// newItem('assets/staff.png', 600, 250)
+
+function newInventory() {
+  let inventory = document.createElement("div");
+  inventory.style.position = "fixed";
+  inventory.style.bottom = "0px";
+  inventory.style.left = "0px";
+  inventory.style.width = "100%";
+  inventory.style.height = "100px";
+  inventory.style.display = "flex";
+  inventory.style.flexDirection = "row";
+  inventory.style.alignItems = "center";
+  inventory.style.justifyContent = "space-evenly";
+  inventory.style.border = "2px solid black";
+  inventory.style.backgroundColor = "brown";
+  document.body.append(inventory);
+  return inventory;
+}
+
+const inventory = newInventory();
